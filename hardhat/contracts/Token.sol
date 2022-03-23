c7c19d1f842ce9d1770363fc6f0d40b0292f5395
Chrakimnas6 <hchen56@u.rochester.edu>
2022-03-24 00:09:37 +0900

    fix

--- hardhat/contracts/Token.sol
+++ hardhat/contracts/Token.sol
@@ -2,19 +2,13 @@
 // It will be used by the Solidity compiler to validate its version.
 pragma solidity ^0.8.9;
 
-import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-// This is the main building block for smart contracts.
 contract MyToken is ERC20, Ownable {
-
-    /**
-     * Contract initialization.
-     *
-     * The `constructor` is executed only once when the contract is created.
-     */
-    constructor() ERC20("MyToken", "MTK") {}
+    constructor() ERC20("MyToken", "MTK") {
+        _mint(msg.sender, 10000 * 10 ** decimals());
+    }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
